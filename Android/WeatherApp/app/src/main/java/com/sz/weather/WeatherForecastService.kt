9871f41060f4517b885c858a7a34536beb8af1c4
Serhii Zashchelkin <sergii.zashchelkin@gmail.com>
2021-03-16 09:12:18 +0200

    Update

--- Android/WeatherApp/app/src/main/java/com/sz/weather/WeatherForecastService.kt
+++ Android/WeatherApp/app/src/main/java/com/sz/weather/WeatherForecastService.kt
@@ -5,6 +5,6 @@ import com.sz.weather.entities.ForecastData
 import retrofit2.http.GET
 
 interface WeatherForecastService {
-    @GET("/data/2.5/onecall?lat=50.549916&lon=30.240000&exclude=current,minutely,daily&units=metric&appid=cee8d1171f06c601ff868efbec254351")
+    @GET("/data/2.5/onecall?lat=50.549916&lon=30.240000&exclude=current,minutely,daily&units=metric&appid=XXXXX")
     fun forecast(): Call<ForecastData>
 }
