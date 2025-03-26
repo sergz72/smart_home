38d1caf2ac2cd5f68758cbceedab85af522d1865
Serhii Zashchelkin <serg.zz@ukr.net>
2025-03-26 18:52:40 +0100

    Updates

--- Rust/drafts/smart_home_user_server/src/command_processor.rs
+++ Rust/drafts/smart_home_user_server/src/command_processor.rs
@@ -1,4 +1,9 @@
+use std::collections::HashMap;
+use std::io::{Error, ErrorKind};
 use smart_home_common::db::DB;
+use smart_home_common::logger::Logger;
+
+const MAX_UNAGGREGATED_DATA_DAYS: i32 = 7;
 
 pub struct CommandProcessor {
     db: DB
@@ -9,7 +14,71 @@ impl CommandProcessor {
         CommandProcessor { db }
     }
 
-    pub fn execute(&self, command: Vec<u8>) -> Vec<u8> {
-        todo!()
+    pub fn execute(&self, logger: &Logger, command: Vec<u8>) -> Result<Vec<u8>, Error> {
+        let command_string = String::from_utf8(command)
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        logger.info(format!("Command: {}", command_string));
+        if !command_string.starts_with("GET ") {
+            return Err(Error::new(ErrorKind::InvalidData, "Invalid command"));
+        }
+        if command_string[4..].starts_with("/sensor_data?") {
+            return self.get_sensor_data(&command_string[17..]);
+        }
+        Err(Error::new(ErrorKind::InvalidData, "Invalid GET operation"))
     }
+
+    fn get_sensor_data(&self, query: &str) -> Result<Vec<u8>, Error> {
+        let parsed = parse_query(query);
+
+        let mut max_points = 1000000;
+        if let Some(max_points_string) = parsed.get("max_points") {
+            max_points = max_points_string.parse()
+                .map_err(|e|Error::new(ErrorKind::InvalidInput, "Invalid max_points value"))?;
+        }
+
+        let data_type = parsed.get("data_type")
+            .ok_or(Error::new(ErrorKind::InvalidInput, "Missing data_type"))?;
+
+        let start_string = parsed.get("start")
+            .ok_or(Error::new(ErrorKind::InvalidInput, "Missing start"))?;
+        let (start_date, start_time) = parse_start(start_string)?;
+
+        let mut end_date = None;
+        let mut end_time = None;
+        if let Some(period_string) = parsed.get("period") {
+            let (end_date_value, end_time_value) =
+                parse_period(period_string, start_date, start_time)?;
+            end_date = Some(end_date_value);
+            end_time = Some(end_time_value);
+        }
+
+        let aggregated = days_between(start_date, end_date) > MAX_UNAGGREGATED_DATA_DAYS;
+
+        let query = aggregated ? build_aggregated_query(start_date, start_time, data_type);
+
+        let mut client = self.db.get_database_connection()?;
+        let rows = client.query("");
+    }
+}
+
+fn days_between(start_date: i32, end_date: Option<i32>) -> i32 {
+    todo!()
+}
+
+fn parse_start(start_string: &String) -> Result<(i32, i32), Error> {
+    todo!()
+}
+
+fn parse_period(period_string: &String, start_date: i32, start_time: i32) -> Result<(i32, i32), Error> {
+    todo!()
+}
+
+fn parse_query(query: &str) -> HashMap<String, String> {
+    query
+        .split('&')
+        .filter_map(|s| {
+            s.split_once('=')
+                .and_then(|t| Some((t.0.to_owned(), t.1.to_owned())))
+        })
+        .collect()
 }
--- Rust/drafts/smart_home_user_server/src/message_processor.rs
+++ Rust/drafts/smart_home_user_server/src/message_processor.rs
@@ -12,6 +12,8 @@ use smart_home_common::db::DB;
 use smart_home_common::logger::Logger;
 use crate::command_processor::CommandProcessor;
 
+const MAX_TIME_DIFFERENCE: u64 = 60;
+
 struct UserMessageProcessor {
     key: [u8; 32],
     command_processor: CommandProcessor
@@ -25,66 +27,107 @@ impl UserMessageProcessor {
 }
 
 impl MessageProcessor for UserMessageProcessor {
-    fn process_message(&self, logger: &Logger, message: &Vec<u8>, time_offset: i64) -> Vec<u8> {
-        let command = self.decrypt(logger, message);
-        if command.is_empty() {
-            return Vec::new();
+    fn process_message(&self, logger: &Logger, message: &Vec<u8>, _time_offset: i64) -> Vec<u8> {
+        match self.process_message_with_result(logger, message) {
+            Ok(result) => result,
+            Err(error) => {
+                logger.error(format!("process_message error: {}", error));
+                Vec::new()
+            }
         }
-        let response = self.command_processor.execute(command);
-        self.encrypt_response(response)
     }
 }
 
 impl UserMessageProcessor {
-    fn decrypt(&self, logger: &Logger, message: &Vec<u8>) -> Vec<u8> {
-        todo!()
+    fn process_message_with_result(&self, logger: &Logger, message: &Vec<u8>)
+        -> Result<Vec<u8>, Error> {
+        let command = self.decrypt(message)?;
+        let response = self.command_processor.execute(logger, command)?;
+        self.encrypt_response(response)
+    }
+
+    fn transform(&self, iv: &[u8], data: &[u8]) -> Result<Vec<u8>, Error> {
+        let mut cipher = ChaCha20::new((&self.key).into(), iv.into());
+        let mut out_vec = vec![0u8; data.len()];
+        let out_bytes = out_vec.as_mut_slice();
+        cipher.apply_keystream_b2b(data, out_bytes)
+            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        Ok(out_vec)
+    }
+
+    fn decrypt(&self, message: &Vec<u8>) -> Result<Vec<u8>, Error> {
+        if message.len() < 13 {
+            return Err(Error::new(ErrorKind::InvalidData, "message is too short"));
+        }
+        let iv = &message[0..12];
+        check_iv(iv)?;
+        let encrypted = &message[12..];
+        self.transform(iv, encrypted)
     }
 
     fn encrypt_response(&self, response: Vec<u8>) -> Result<Vec<u8>, Error> {
-        let compressed = self.compress(response)?;
+        let compressed = compress(response)?;
         self.encrypt(compressed)
     }
 
-    fn compress(&self, data: Vec<u8>) -> Result<Vec<u8>, Error> {
-        let mut compressor = BzEncoder::new(data.as_slice(), Compression::best());
-        let mut result = Vec::new();
-        compressor.read_to_end(&mut result)?;
-        Ok(result)
-    }
-
     fn encrypt(&self, data: Vec<u8>) -> Result<Vec<u8>, Error> {
-        let iv = self.build_iv()?;
-        let mut cipher = ChaCha20::new((&self.key).into(), (&iv).into());
+        let iv = build_iv()?;
         let bytes = data.as_slice();
-        let mut out_vec = vec![0u8; bytes.len()];
-        let out_bytes = out_vec.as_mut_slice();
-        cipher.apply_keystream_b2b(bytes, out_bytes)
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        let mut out_vec = self.transform(&iv, bytes)?;
         let mut result = Vec::from(iv);
         result.append(&mut out_vec);
         Ok(result)
     }
+}
 
-    fn build_iv(&self) -> Result<[u8; 12], Error> {
-        let mut random_part = [0u8; 4];
-        OsRng.try_fill_bytes(&mut random_part)
-            .map_err(|e| Error::new(ErrorKind::Other, e))?;
-        let mut iv = [0u8; 12];
-        let now = SystemTime::now().duration_since(UNIX_EPOCH)
-            .map_err(|e| Error::new(ErrorKind::Other, e))?;
-        iv[0..4].copy_from_slice(&random_part);
-        let mut time_bytes = now.as_secs().to_le_bytes();
-        time_bytes[0] ^= random_part[0];
-        time_bytes[1] ^= random_part[1];
-        time_bytes[2] ^= random_part[2];
-        time_bytes[3] ^= random_part[3];
-        time_bytes[4] ^= random_part[0];
-        time_bytes[5] ^= random_part[1];
-        time_bytes[6] ^= random_part[2];
-        time_bytes[7] ^= random_part[3];
-        iv[4..12].copy_from_slice(&time_bytes);
-        Ok(iv)
+fn compress(data: Vec<u8>) -> Result<Vec<u8>, Error> {
+    let mut compressor = BzEncoder::new(data.as_slice(), Compression::best());
+    let mut result = Vec::new();
+    compressor.read_to_end(&mut result)?;
+    Ok(result)
+}
+
+fn check_iv(iv: &[u8]) -> Result<(), Error> {
+    let random_part = &iv[0..4];
+    let mut time_bytes = [0u8; 8];
+    time_bytes.copy_from_slice(&iv[4..12]);
+    time_bytes[0] ^= random_part[0];
+    time_bytes[1] ^= random_part[1];
+    time_bytes[2] ^= random_part[2];
+    time_bytes[3] ^= random_part[3];
+    time_bytes[4] ^= random_part[0];
+    time_bytes[5] ^= random_part[1];
+    time_bytes[6] ^= random_part[2];
+    time_bytes[7] ^= random_part[3];
+    let now = SystemTime::now().duration_since(UNIX_EPOCH)
+        .map_err(|e| Error::new(ErrorKind::Other, e))?.as_secs();
+    let time = u64::from_le_bytes(time_bytes);
+    if (now >= time && now - time > MAX_TIME_DIFFERENCE) ||
+        (now < time && time - now > MAX_TIME_DIFFERENCE) {
+        return Err(Error::new(ErrorKind::InvalidData, "incorrect nonce"));
     }
+    Ok(())
+}
+
+fn build_iv() -> Result<[u8; 12], Error> {
+    let mut random_part = [0u8; 4];
+    OsRng.try_fill_bytes(&mut random_part)
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+    let mut iv = [0u8; 12];
+    let now = SystemTime::now().duration_since(UNIX_EPOCH)
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+    iv[0..4].copy_from_slice(&random_part);
+    let mut time_bytes = now.as_secs().to_le_bytes();
+    time_bytes[0] ^= random_part[0];
+    time_bytes[1] ^= random_part[1];
+    time_bytes[2] ^= random_part[2];
+    time_bytes[3] ^= random_part[3];
+    time_bytes[4] ^= random_part[0];
+    time_bytes[5] ^= random_part[1];
+    time_bytes[6] ^= random_part[2];
+    time_bytes[7] ^= random_part[3];
+    iv[4..12].copy_from_slice(&time_bytes);
+    Ok(iv)
 }
 
 pub fn build_message_processor(key: [u8; 32], db: DB)
